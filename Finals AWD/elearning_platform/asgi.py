a1c6a5b7be07e29780aa6045a60d968fed56a09d
Jesmond Lee <jesmondlkw@gmail.com>
2025-08-29 02:48:49 +0800

    Use CompressedStaticFilesStorage to avoid DRF font manifest errors4

--- Finals AWD/elearning_platform/asgi.py
+++ Finals AWD/elearning_platform/asgi.py
@@ -1,19 +1,14 @@
-"""
-ASGI config for elearning_platform project.
-
-Exposes the ASGI callable as a module-level variable named ``application``.
-
-See: https://channels.readthedocs.io/en/latest/deploying.html
-"""
 import os
 import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from chat.routing import websocket_urlpatterns
 
+# Configure settings **before** importing anything that touches Django models/apps
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elearning_platform.settings")
 django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
